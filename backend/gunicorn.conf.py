3b2a538687e62e00f0d086cf7586d3c60159ffdd
Rania Maamer <raniamaamer2001@gmail.com>
2026-04-17 21:36:34 +0100

    refactor: fix minor SonarQube issues (unused vars, f-strings, set comprehension)

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -2,6 +2,6 @@ bind = "0.0.0.0:8000"
 workers = 3
 worker_class = "sync"
 
-def post_fork(server, worker):
+def post_fork(_server, _worker):
     import os
     os.environ["GUNICORN_WORKER"] = "true"
